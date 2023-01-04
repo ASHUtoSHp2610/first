939a014a7892345519e0b1a01ebf8cfb8271a7c3
ASHUtoSHp2610 <88331547+ASHUtoSHp2610@users.noreply.github.com>
2023-01-04 22:53:38 +0530

    first commit

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -3,43 +3,59 @@ package hackerrank;
 import java.util.HashMap;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
-    /**
-     * TODO: Create a map of Furniture items to order quantities
-     */
+    private HashMap<Furniture, Integer> order;
 
-    /**
-     * Initialize a new mapping of Furniture types to order quantities.
-     */
-    FurnitureOrder() {
-        // TODO: Complete the constructor
-    }
+  public FurnitureOrder() {
+    order = new HashMap<Furniture, Integer>();
+  }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+        if (order.containsKey(type)) {
+            order.put(type, order.get(type) + count);
+          } else {
+            order.put(type, count);
+          }
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return order;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        float totalCost = 0;
+    for (Furniture type : order.keySet()) {
+      totalCost += getTypeCost(type);
+    }
+    return totalCost;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        if (order.containsKey(type)) {
+            return order.get(type);
+          } else {
+            return 0;
+        }
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        if (order.containsKey(type)) {
+            return type.getCost() * order.get(type);
+          } else {
+            return 0;
+          }
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
-        return -1;
+        int totalQuantity = 0;
+    for (Furniture type : order.keySet()) {
+      totalQuantity += getTypeCount(type);
+    }
+    return totalQuantity;
     }
 }
